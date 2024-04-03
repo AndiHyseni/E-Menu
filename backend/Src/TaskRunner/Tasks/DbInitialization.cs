43ad2acc8692dc47b5604930e48cd89c5a643a40
berat422 <75537316+berat422@users.noreply.github.com>
2024-04-04 00:39:48 +0200

    Creating option for seeding
    seeding

---
+++ backend/Src/TaskRunner/Tasks/DbInitialization.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskRunner.Tasks
+{
+    public class DbInitialization
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        public DbInitialization(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task Run()
+        {
+           await _appDbContext.Database.EnsureDeletedAsync(CancellationToken.None);
+           await _appDbContext.Database.EnsureCreatedAsync(CancellationToken.None);
+        }
+    }
+}
